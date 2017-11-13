3a3ad5d035b5c839cc1e1298b32861a5800a07b8
Christian Savard <me@savardc.com>
2017-11-13 14:52:13 -0500

    Initial update

--- src/components/KeyCommandController.js
+++ src/components/KeyCommandController.js
@@ -2,6 +2,7 @@ import invariant from 'invariant';
 import {List} from 'immutable';
 import React from 'react';
 import PropTypes from 'prop-types';
+import createReactClass from 'create-react-class';
 import {EditorState} from 'draft-js';
 
 const providedProps = {
@@ -10,7 +11,7 @@ const providedProps = {
   handleKeyCommand: PropTypes.func
 };
 
-const KeyCommandController = (Component) => React.createClass({
+const KeyCommandController = (Component) => createReactClass({
   displayName: `KeyCommandController(${Component.displayName})`,
 
   propTypes: {
